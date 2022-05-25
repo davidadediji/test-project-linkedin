50af4ea25190345c1d4050d30cf0b7683d6ad9a8
Emma Paris <davidadediji@gmail.com>
2022-05-25 13:04:57 +0100

    remove keys

--- test/test-1.js
+++ test/test-1.js
@@ -6,8 +6,8 @@ describe('LinkedIn Login', ()=>{
     it('should login with valid email and password', async ()=>{
         let driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://www.linkedin.com/login');
-        await driver.findElement(By.id('username')).sendKeys('davidadediji@gmail.com');
-        await driver.findElement(By.id('password')).sendKeys('praise41');
+        await driver.findElement(By.id('username')).sendKeys('');
+        await driver.findElement(By.id('password')).sendKeys('');
 
         let confirmLogin = await driver.findElement(By.id())
 
