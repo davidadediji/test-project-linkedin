cd18aedf889da8d91ba54a0cfde8f74820a9bb4f
David Adediji <davidadediji@gmail.com>
2022-05-28 21:40:15 +0100

    modify README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # LinkedIn Authentication Test
 
-
+To run the test copy contents in env-example, create a `.env` file and add to it along with your credentials
+Ensure to create a .gitignore file and add include theclea .env
 
 ### Requirements
 - Selenium webdriver
@@ -38,10 +39,11 @@ mocha --no-timeouts --parallel reporter mochawesome
 ```
 
 ### Test Cases
-- Test with valid email and password   -- Positive
-- Test with invalid email and valid password  --Negative
+- Test with valid email and password   -- Positive [+]
+- Test with invalid email and valid password  --Negative [+]
 - Test with valid email and invalid password   -- Negative
-- Test the Login page against SQL injection attack -- negative
+- Test the Login page with valid phone no with country code and password -- positive
+- Test the Login page with valid phone no with no country code and valid password -- negative
 - Test the login page for both, when the field is blank and Submit button is clicked -- Negative
 ### Conclusion
 Test
--- test/test-1.js
+++ test/test-1.js
@@ -6,9 +6,25 @@ dotenv.config();
 
 let email = process.env.VALID_EMAIL;
 let password = process.env.VALID_PASSWORD;
+let wrongEmail = process.env.INVALID_EMAIL;
+let invalidPassword = process.env.INVALID_PASSWORD;
+let phoneNoCode = process.env.PHONE_NO_COUNTRY_CODE;
+let phoneCode = process.env.PHONE_COUNTRY_CODE;
 
-describe('LinkedIn Login', ()=>{
-    it('should login with valid email and password', async ()=>{
+
+describe('LinkedIn Login', ()=>{ 
+    it('should throw an error when all field is blank', async ()=>{
+        let driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('https://www.linkedin.com/login');
+        await driver.findElement(By.id('username')).sendKeys(``);
+        await driver.findElement(By.id('password')).sendKeys(``);
+        await driver.findElement(By.className('btn__primary--large')).click();
+        
+        let newtext = await driver.findElement(By.id("error-for-username")).getText().then((value)=>value)
+        chai.expect(newtext).to.eql('Please enter an email address or phone number');
+        await driver.close();
+    })
+    it('should login with valid credentials', async ()=>{
         let driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://www.linkedin.com/login');
         await driver.findElement(By.id('username')).sendKeys(`${email}`);
@@ -17,34 +33,46 @@ describe('LinkedIn Login', ()=>{
 
         await driver.close();
     })
-
-    it('should login with valid email and password', async ()=>{
+    it('should not login with wrong email,valid password', async ()=>{
         let driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://www.linkedin.com/login');
-        await driver.findElement(By.id('username')).sendKeys(`${email}`);
+        await driver.findElement(By.id('username')).sendKeys(`${wrongEmail}`);
         await driver.findElement(By.id('password')).sendKeys(`${password}`);
         await driver.findElement(By.className('btn__primary--large')).click();
-
+        
+        let newtext = await driver.findElement(By.id("error-for-password")).getText().then((value)=>value)
+        chai.expect(newtext).to.eql("That's not the right password. Try again or sign in with a one-time link");
         await driver.close();
     })
 
-    it('should login with valid email and password', async ()=>{
+    it('should not login with valid email, invalid password', async ()=>{
         let driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://www.linkedin.com/login');
         await driver.findElement(By.id('username')).sendKeys(`${email}`);
-        await driver.findElement(By.id('password')).sendKeys(`${password}`);
+        await driver.findElement(By.id('password')).sendKeys(`${invalidPassword}`);
         await driver.findElement(By.className('btn__primary--large')).click();
-
+        
+        let newtext = await driver.findElement(By.id("error-for-username")).getText().then((value)=>value)
+        chai.expect(newtext).to.eql('Please enter an email address or phone number');
         await driver.close();
     })
 
-    it('should login with valid email and password', async ()=>{
+    it('should login with phone(no country code), valid password', async ()=>{
         let driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://www.linkedin.com/login');
-        await driver.findElement(By.id('username')).sendKeys(`${email}`);
+        await driver.findElement(By.id('username')).sendKeys(`${phoneNoCode}`);
         await driver.findElement(By.id('password')).sendKeys(`${password}`);
         await driver.findElement(By.className('btn__primary--large')).click();
-
         await driver.close();
     })
+
+    it('should not login with phone (country code), valid password', async ()=>{
+        let driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('https://www.linkedin.com/login');
+        await driver.findElement(By.id('username')).sendKeys(`${phoneCode}`);
+        await driver.findElement(By.id('password')).sendKeys(`${password}`);
+        await driver.findElement(By.className('btn__primary--large')).click();
+        await driver.close();
+    })
+
 })
