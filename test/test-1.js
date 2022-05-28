fc7e1501127151c9ac45419c486219c5a6f058fc
David Adediji <davidadediji@gmail.com>
2022-05-28 19:19:47 +0100

    add new test

--- test/test-1.js
+++ test/test-1.js
@@ -1,15 +1,19 @@
 const {Builder, By} = require('selenium-webdriver');
 var chai = require('chai');
+const dotenv = require('dotenv')
 
+dotenv.config();
+
+let email = process.env.VALID_EMAIL;
+let password = process.env.VALID_PASSWORD;
 
 describe('LinkedIn Login', ()=>{
     it('should login with valid email and password', async ()=>{
         let driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://www.linkedin.com/login');
-        await driver.findElement(By.id('username')).sendKeys('');
-        await driver.findElement(By.id('password')).sendKeys('');
-
-        let confirmLogin = await driver.findElement(By.id())
+        await driver.findElement(By.id('username')).sendKeys(`${email}`);
+        await driver.findElement(By.id('password')).sendKeys(`${password}`);
+        await driver.findElement(By.className('btn__primary--large')).click();
 
         await driver.close();
     })
